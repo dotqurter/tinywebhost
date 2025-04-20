7e7d5927f9ab8efc304a5026014575040dd34075
dotqurter <rigglemangamers@gmail.com>
2025-04-20 01:21:50 -0400

    small changes to header parsing

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
-use std::{ io::{prelude::*, BufReader, ErrorKind}, net::{TcpListener, TcpStream}, fs, };
+use std::{ io::{prelude::*, BufReader, ErrorKind}, net::{TcpListener, TcpStream}, fs, thread};
 const ROOT_FOLDER: &str = "/var/web";
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:80").unwrap();
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        handle_connection(stream);
+        thread::spawn(|| {
+            handle_connection(stream);
+        });
     }
 }
 
@@ -13,20 +15,23 @@ fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream);
     let http_request: Vec<String> = buf_reader
         .lines()
-        .map(|result| result.unwrap())
+        .map(|result| match result {
+            Ok(a) => a,
+            Err(_) => String::new(),
+        })
         .take_while(|line| !line.is_empty())
         .collect();
 
-    let requested_file = if !http_request.is_empty() {
-        http_request[0]
-        .strip_prefix("GET ").unwrap()
-        .strip_suffix(" HTTP/1.1").unwrap()
+    println!("{:?}", http_request);
+    let requested_file = if !http_request.is_empty() && http_request[0].contains("GET") {
+        http_request[0].rsplit(' ').nth(1).unwrap()
     } else { return };
 
-    let mut file: String = requested_file.to_string();
-    if requested_file == "/" {
-        file = String::from("/index.html");
-    }
+    let file = if requested_file == "/" {
+        "/index.html"
+    } else {
+        requested_file
+    };
 
     let err_404_page = format!("{ROOT_FOLDER}/404.html");
     let path = format!("{ROOT_FOLDER}{file}");
@@ -36,9 +41,9 @@ fn handle_connection(mut stream: TcpStream) {
             ( "HTTP/1.1 200 OK\r\n", data )
         },
         Err(err) => {
+            eprintln!("Error opening {path}: {err}");
             match err.kind() {
                 ErrorKind::NotFound => { 
-                    println!("Connection requested nonexistent file, {}", path); 
                     if file.ends_with(".html") {
                         ( "HTTP/1.1 404 NOT FOUND\r\n", fs::read(err_404_page).unwrap() )
                     } else {
@@ -46,6 +51,7 @@ fn handle_connection(mut stream: TcpStream) {
                     }
                 },
                 _ => {
+
                     ( "HTTP/1.1 500 INTERNAL SERVER ERROR\r\n", Vec::new() )
                 }
             }
@@ -53,14 +59,13 @@ fn handle_connection(mut stream: TcpStream) {
     };
 
     let length = data.len();
-    let mut contents: Vec<u8> = Vec::new();
+    let mut response: Vec<u8> = status_line.into();
 
     if length > 0 {
-        contents = format!("Content-Length: {length}\r\n\r\n").into();
+        let mut contents: Vec<u8> = format!("Content-Length: {length}\r\n\r\n").into();
         contents.extend(data);
+        response.extend(contents);
     }
 
-    let mut response: Vec<u8> = status_line.into();
-    response.extend(contents);
     stream.write_all(response.as_slice()).unwrap();
 }
